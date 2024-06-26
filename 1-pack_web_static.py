7150539b2b4e02da2656b04fad61afa0b3b88c4b
root <root@MMENYENE-IDIONG>
2024-06-26 23:56:07 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
+""" Fabfile that create a .tgz archive from
+the contents of web_static folder"""
+
+# if __name__ == '__main__':
+from fabric.api import local
 from datetime import datetime
-from fabric.api import local, runs_once
 
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
+    """Pack all the contents in the web_static directory
+    as a tar archive"""
+
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+        local("mkdir -p versions")
+        time = datetime.now()
+        date_string = '%Y%m%d%H%M%S'
+        date = time.strftime(date_string)
+
+        file_path = "versions/web_static_{}.tgz".format(date)
+        local("tar -czvf {} web_static".format(file_path))
+        return file_path
+
     except Exception:
-        output = None
-    return output
+        return None
